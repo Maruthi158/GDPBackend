c24b833d6fe6e2c9073bedb9b98a6035163c8c10
RonaldoS542274 <77812398+RonaldoS542274@users.noreply.github.com>
2021-11-28 23:19:30 -0600

    wirtten method for authentication

--- methods/authentication.js
+++ methods/authentication.js
@@ -1,3 +1,16 @@
 const jwt = require('jsonwebtoken')
 const user = require('../collection')
 
+const Authentication = async (req, res, next) => {
+try {
+        if (typeof(req.cookies.jwtToken) === "undefined") {
+            console.log("page is not found")
+            res.json({success:0})
+        }else {
+          const cookieToken = req.cookies.jwtToken;
+          
+        }
+  
+  
+}
+module.exports = Authentication
