44ecbfcb29c0cd1fee2360b9ed1ef09fd3a99ed8
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-11-29 12:13:26 -0600

    Modifying authentication.js

--- methods/authentication.js
+++ methods/authentication.js
@@ -2,26 +2,24 @@ const jwt = require('jsonwebtoken')
 const user = require('../collection')
 
 const Authentication = async (req, res, next) => {
-try {
+    try {
         if (typeof(req.cookies.jwtToken) === "undefined") {
             console.log("page is not found")
             res.json({success:0})
-        }else {
-          const cookieToken = req.cookies.jwtToken;
-          const verifyToken = jwt.verify(cookieToken, "BearcatStudyBuddyProject")
+        } else {
+            const cookieToken = req.cookies.jwtToken;
+            const verifyToken = jwt.verify(cookieToken, "BearcatStudyBuddyProject")
 
-          const verifyUser = await user.findOne({ _id: verifyToken._id, token: cookieToken })
-          if (!verifyUser) {
+            const verifyUser = await user.findOne({ _id: verifyToken._id, token: cookieToken })
+            if (!verifyUser) {
                 res.json({success:0})
             }
             req.user = verifyUser
             next()
-          
         }
-        } catch (err) {
+    } catch (err) {
         console.log(err)
     }
-  
-  
 }
+
 module.exports = Authentication
